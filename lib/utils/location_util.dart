e2cab07016be83cc2757afcf6a2b6cd394237eb3
absjuniordev <abs.junnior@hotmail.com>
2023-02-06 23:20:41 -0300

    Mostrando Imagem Estática do Mapa

---
+++ lib/utils/location_util.dart
@@ -0,0 +1,10 @@
+const GOOGLE_API_KEY = 'AIzaSyDPLDJ1TExrJok_XAnmi7gT_sE6bQ6DkwQ';
+
+class LocationUtil {
+  static String generateLocationPreviewImage({
+    required double latitude,
+    required double longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
